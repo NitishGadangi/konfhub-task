4e0022b7f41b953dcfb49d29a4bfa57d523f7685
NitishGadangi <nitishgadangi@gmail.com>
2020-05-11 18:47:03 +0530

    added working logic

--- task.py
+++ task.py
@@ -13,6 +13,16 @@ def get_json_response():
 	json_response=req.json()
 	return json_response
 
+def get_all_conferences(json_response):
+	all_conference=[]
+	paid_conf=json_response['paid']
+	print(f"\n....{len(paid_conf)} paid conferences")
+	# all_conference.append(paid_conf)
+	free_conf=json_response['free']
+	print(f"....{len(free_conf)} free conferences")
+	# all_conference.append(free_conf)
+	return paid_conf+free_conf
+
 def print_content(all_conferences):
 	for conference in all_conferences:
 		req_values=[conference['confName'],conference['confStartDate'],conference['city'],conference['state'],conference['country'],conference['entryType'],conference['confRegUrl']]
@@ -21,17 +31,13 @@ def print_content(all_conferences):
 		print(output)
 
 def remove_exact_and_semantic_duplicates(all_conferences):
-	filtered=[]
 	duplicates=[]
 	for i in range(0,len(all_conferences)):
 		for j in range(0,i):
 			diff=difference_dict(all_conferences[i],all_conferences[j])
-			print(diff)
 			if diff<=3:
 				duplicates.append(all_conferences[i])
-			else:
-				filtered.append(all_conferences[i])
-	return filtered,duplicates
+	return duplicates
 
 def generate_csv(all_conferences):
 	with open('output.csv', 'w') as f:
@@ -39,8 +45,33 @@ def generate_csv(all_conferences):
 		w.writerow(all_conferences[0].keys())
 		for conf in all_conferences:
 			w.writerow(conf.values())
-	print("\n\n***output.csv exported to current direcrory***")
+	print("\n***output.csv exported to current working direcrory***")
 
 
 if __name__ == "__main__":
 	print("Hello World")
+
+	print("\n....Fetching Json Response from the API...")
+	json_response=get_json_response()
+	all_conferences=get_all_conferences(json_response)
+
+	print("\n....searching exact and semantic duplicates...")
+	duplicates=remove_exact_and_semantic_duplicates(all_conferences)
+	print(f"\n....{len(duplicates)} duplicates found")
+	print(f"....{len(all_conferences)} total conferences")
+	print("\n....Filtering Done......\n\n")
+
+	print("---Select an Option--")
+	print("-> 1. Print the contents in a human readable format")
+	print("-> 2. Print Duplicates")
+	print("-> 3. Export the contents to csv")
+	option=int(input("option(1/2/3): "))
+	if option is 1:
+		print_content(all_conferences)
+	elif option is 2:
+		print_content(duplicates)
+	elif option is 3:
+		generate_csv(all_conferences)
+	else:
+		print("\nYou have entered a wrong input\n\n restart the program again")
+
